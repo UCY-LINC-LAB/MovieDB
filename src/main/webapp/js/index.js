efe69163ea1030621a36d58a309fbbd5302419da
dpasch01 <dpasch01@cs.ucy.ac.cy>
2017-01-10 15:59:46 +0200

    Added sauce in UI.

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,138 +1,115 @@
-data = [
-	{
-		"make": "Gibson", // Name
-		"model": "Les Paul", // Rating
-		"type": "Electric", // Year
-		"price": "$3,000", // Lang
-		"content_rating": "P12",
-		"country": "USA",
-	}
-];
-
 var products = "";
-var makes = ""; // Name
-var models = ""; // Rating
-var types = ""; // Year
+var makes = "";
+var models = "";
+var types = "";
 var content_rating = "";
-var languages= "";
+var languages = "";
 var countries = "";
 
+$('#myModal').on('show.bs.modal', function (e) {
+	var imdb = $(e.relatedTarget).attr("data-imdb");
+	var title = $(e.relatedTarget).attr("data-title");
+	var year = $(e.relatedTarget).attr("data-year");
+	var score = $(e.relatedTarget).attr("data-score");
+	var duration = $(e.relatedTarget).attr("data-duration");
+	var director = $(e.relatedTarget).attr("data-director");
+	var actors = $(e.relatedTarget).attr("data-actors");
+	var genres = $(e.relatedTarget).attr("data-genres");
+	var content = $(e.relatedTarget).attr("data-content");
+	var country = $(e.relatedTarget).attr("data-country");
+	var language = $(e.relatedTarget).attr("data-language");
+
+	$('#myModal .modal-title').html(title);
+	$('#myModal .modal-body .movie-item .movie-info .year').html(year);
+	$('#myModal .modal-body .movie-item .movie-info .director').html(director);
+	$('#myModal .modal-body .movie-item .movie-info .score').html(score);
+	$('#myModal .modal-body .movie-item .movie-info .duration').html(duration);
+	$('#myModal .modal-body .movie-item .movie-info .actors').html(actors);
+	$('#myModal .modal-body .movie-item .movie-info .genres').html(genres);
+	$('#myModal .modal-body .movie-item .movie-info .content-rating').html(content);
+	$('#myModal .modal-body .movie-item .movie-info .language').html(language);
+	$('#myModal .modal-body .movie-item .movie-info .country').html(country);
+	$('#myModal .modal-body .imdb-redirect').attr('href', imdb);
+})
 
 $.ajax({
-  url: "http://localhost:8080/MovieDatabase/rest/utils/years/",
-  cache: false,
-  success: function(data){
-// data.start_date
-// data.end_date
-// data.year
-// data.content_rating
-// data.country
-	  data.years.sort(function(a,b){
-		  return b-a;
-	  });
-  	for (var i = 0; i < data.years.length; i++) {
-  		var type = data.years[i];
-  		if (types.indexOf("<option value='"+type+"'>"+type+"</option>") == -1) {
-			types += "<option value='"+type+"'>"+type+"</option>";
-		}
-  	}
-// for (var i = 0; i < data.length; i++) {
-// var make = data[i].make,
-// model = data[i].model,
-// type = data[i].type,
-// price = data[i].price,, // Lang
-// content_rating = data[i].content_rating;
-// country = data[i].country;
-//
-// //create product cards
-// products += "<div class='col-sm-4 product' data-make='"+make+"'
-// data-model='"+model+"' data-type='"+type+"' data-price='"+rawPrice+"'><div
-// class='product-inner text-center'>Name: "+make +"<br />Rating: "+model+"<br
-// />Year: "+type+"<br />Language: "+price+"<br />Content Rating:
-// "+content_rating+"<br />Country: "+country+"</div></div>";
-//
-// //create dropdown of Year
-// if (types.indexOf("<option value='"+type+"'>"+type+"</option>") == -1) {
-// types += "<option value='"+type+"'>"+type+"</option>";
-// }
-//
-// //create dropdown of content_rating
-// if (types.indexOf("<option
-// value='"+content_rating+"'>"+content_rating+"</option>") == -1) {
-// types += "<option value='"+content_rating+"'>"+content_rating+"</option>";
-// }
-//
-// //create dropdown of country
-// if (types.indexOf("<option value='"+country+"'>"+country+"</option>") == -1)
-// {
-// types += "<option value='"+country+"'>"+country+"</option>";
-// }
-// }
-//
-// $("#products").html(products);
-// $(".filter-make").append(makes);
-// $(".filter-model").append(models);
-  	$(".filter-start-date").append(types);
-
-  	$(".filter-end-date").append(types);
-  }
-});
-
-$.ajax({
-  url: "http://localhost:8080/MovieDatabase/rest/utils/languages/",
-  cache: false,
-  success: function(data){
-	data.languages.sort();
-  	for (var i = 0; i < data.languages.length; i++) {
-  		var type = data.languages[i];
-  		if (languages.indexOf("<option value='"+type+"'>"+type+"</option>") == -1) {
-  			languages += "<option value='"+type+"'>"+type+"</option>";
-		}
-  	}
-
-  	$(".filter-language").append(languages);
-  }
-});
-
-
-$.ajax({
-  url: "http://localhost:8080/MovieDatabase/rest/utils/content-ratings/",
-  cache: false,
-  success: function(data){
-	data.content_ratings.sort();
-  	for (var i = 0; i < data.content_ratings.length; i++) {
-  		var type = data.content_ratings[i];
-  		if (content_rating.indexOf("<option value='"+type+"'>"+type+"</option>") == -1) {
-  			content_rating += "<option value='"+type+"'>"+type+"</option>";
-		}
-  	}
-
-  	$(".filter-content").append(content_rating);
-  }
-});
-
-$.ajax({
-	  url: "http://localhost:8080/MovieDatabase/rest/utils/countries/",
-	  cache: false,
-	  success: function(data){
-		data.countries.sort();
-	  	for (var i = 0; i < data.countries.length; i++) {
-	  		var type = data.countries[i];
-	  		if (countries.indexOf("<option value='"+type+"'>"+type+"</option>") == -1) {
-	  			countries += "<option value='"+type+"'>"+type+"</option>";
+	url : "http://localhost:8080/MovieDatabase/rest/utils/years/",
+	cache : false,
+	success : function(data) {
+		data.years.sort(function(a, b) {
+			return b - a;
+		});
+		for (var i = 0; i < data.years.length; i++) {
+			var type = data.years[i];
+			if (types.indexOf("<option value='" + type + "'>" + type
+					+ "</option>") == -1) {
+				types += "<option value='" + type + "'>" + type + "</option>";
 			}
-	  	}
+		}
 
-	  	$(".filter-country").append(countries);
-	  }
-	});
+		$(".filter-start-date").append(types);
+		$(".filter-end-date").append(types);
+	}
+});
+
+$.ajax({
+	url : "http://localhost:8080/MovieDatabase/rest/utils/languages/",
+	cache : false,
+	success : function(data) {
+		data.languages.sort();
+		for (var i = 0; i < data.languages.length; i++) {
+			var type = data.languages[i];
+			if (languages.indexOf("<option value='" + type + "'>" + type
+					+ "</option>") == -1) {
+				languages += "<option value='" + type + "'>" + type
+						+ "</option>";
+			}
+		}
+
+		$(".filter-language").append(languages);
+	}
+});
+
+$.ajax({
+	url : "http://localhost:8080/MovieDatabase/rest/utils/content-ratings/",
+	cache : false,
+	success : function(data) {
+		data.content_ratings.sort();
+		for (var i = 0; i < data.content_ratings.length; i++) {
+			var type = data.content_ratings[i];
+			if (content_rating.indexOf("<option value='" + type + "'>" + type
+					+ "</option>") == -1) {
+				content_rating += "<option value='" + type + "'>" + type
+						+ "</option>";
+			}
+		}
+
+		$(".filter-content").append(content_rating);
+	}
+});
+
+$.ajax({
+	url : "http://localhost:8080/MovieDatabase/rest/utils/countries/",
+	cache : false,
+	success : function(data) {
+		data.countries.sort();
+		for (var i = 0; i < data.countries.length; i++) {
+			var type = data.countries[i];
+			if (countries.indexOf("<option value='" + type + "'>" + type
+					+ "</option>") == -1) {
+				countries += "<option value='" + type + "'>" + type
+						+ "</option>";
+			}
+		}
+
+		$(".filter-country").append(countries);
+	}
+});
 
 var filtersObject = {};
 
-// on filter change
-$(".filter").on("change",function() {
-	var filterName = $(this).data("filter"),
-		filterVal = $(this).val();
+$(".filter").on("change", function() {
+	var filterName = $(this).data("filter"), filterVal = $(this).val();
 
 	if (filterVal == "") {
 		delete filtersObject[filterName];
@@ -142,10 +119,10 @@ $(".filter").on("change",function() {
 
 	var filters = "";
 
-	for (var key in filtersObject) {
-	  	if (filtersObject.hasOwnProperty(key)) {
-			filters += "[data-"+key+"='"+filtersObject[key]+"']";
-	 	 }
+	for ( var key in filtersObject) {
+		if (filtersObject.hasOwnProperty(key)) {
+			filters += "[data-" + key + "='" + filtersObject[key] + "']";
+		}
 	}
 
 	if (filters == "") {
@@ -156,82 +133,115 @@ $(".filter").on("change",function() {
 	}
 });
 
-$( document ).ajaxStart(function() {
-  $( ".spinner" ).removeClass("hidden");
+$(document).ajaxStart(function() {
+	$(".spinner").removeClass("hidden");
 });
-$( document ).ajaxComplete(function() {
-  $( ".spinner" ).addClass("hidden");
-});
-
-$( document ).ready(function() {
- $.ajax({
- type: 'GET',
- url: "http://localhost:8080/MovieDatabase/rest/movies/list",
- success: function(data){
- for (var i = 0; i < data.length; i++) {
-	 $('ul.list-group').append('<li class="list-group-item"><span class="movie-title">'
-			   + data[i].title +'</span><span class="label label-primary">'
-			   + data[i].year + '</span><span class="label label-success">'
-			   + data[i].score + '</span><span class="label label-info">'
-			   + data[i].language + '</span><span class="label label-warning">'
-			   + data[i].genres + '</span><a class="imdb-link" href="'
-			   + data[i].imdb +'"><img class="imdb-logo" src="imdb.png"/></a></li>');
-
- }}
- });});
-
-// on search form submit
-$("#search-form").submit(function(e) {
-	$('ul.list-group').html("");
-	e.preventDefault();
-	query = $("#search-form input").val().toLowerCase();
-
-	min_rating = $(".ui-slider-handle:eq(0)").text();
-	max_rating = $(".ui-slider-handle:eq(1)").text();
-
-	start_date = $(".filter-start-date").val();
-	end_date = $(".filter-end-date").val();
-
-	lang = $(".filter-language").val();
-
-	content_rating = $(".filter-content-rating").val();
-
-	country = $(".filter-country").val();
-
-	 $.ajax({
-	   type: 'GET',
-	   url: "http://localhost:8080/MovieDatabase/rest/movies/filter?yearfrom=" + start_date +"&yearto=" + end_date + "&ratingfrom=" + min_rating + "&ratingto=" + max_rating + "&keywords=" + query + "&content_rating=" + content_rating + "&language=" + lang,
-	   success: function(data){
-		   for (var i = 0; i < data.length; i++) {
-			   $('ul.list-group').append('<li class="list-group-item"><span class="movie-title">'
-					   + data[i].title +'</span><span class="label label-primary">'
-					   + data[i].year + '</span><span class="label label-success">'
-					   + data[i].score + '</span><span class="label label-info">'
-					   + data[i].language + '</span><span class="label label-warning">'
-					   + data[i].genres + '</span><a class="imdb-link" href="'
-					   + data[i].imdb +'"><img class="imdb-logo" src="imdb.png"/></a></li>');
-		  	}
-
-	   },
-	 });
+$(document).ajaxComplete(function() {
+	$(".spinner").addClass("hidden");
 });
 
+$(document)
+		.ready(
+				function() {
+					$
+							.ajax({
+								type : 'GET',
+								url : "http://localhost:8080/MovieDatabase/rest/movies/list",
+								success : function(data) {
+									for (var i = 0; i < data.length; i++) {
+										$('ul.list-group')
+												.append(
+													'<a href="#" data-toggle="modal" data-target="#myModal"	data-imdb="'+data[i].imdb+'" data-director="'+data[i].director+'" data-duration="'+data[i].duration+'" data-actors="'+data[i].actors+'" data-genres="'+data[i].genres+'"	data-title="'+data[i].title+'" data-language="'+data[i].language+'"	data-country="'+data[i].country+'" data-content="'+data[i].content_rating+'"data-score="'+data[i].score+'"data-year="'+data[i].year+'" class="list-group-item"><span class="movie-title">'+data[i].title+'</span><span class="label label-primary">'+data[i].year+'</span><span class="label label-success">'+data[i].score+'</span>	<span class="label label-info">'+data[i].language+'</span></a>'
+												);
 
-$('#slider').slider({
-	range: true,
-	min: 0,
-	max: 10,
-	values: [0, 10],
-	slide: function(event, ui) {
-		$('.ui-slider-handle:eq(0) .price-range-min').html('' + ui.values[ 0 ]);
-		$('.ui-slider-handle:eq(1) .price-range-max').html('' + ui.values[ 1 ]);
-		$('.price-range-both').html('<i>' + ui.values[ 0 ] + '  </i>' + ui.values[ 1 ] );
+									}
+								}
+							});
+				});
 
-		$('.price-range-min, .price-range-max').css('opacity', '1');
-		$('.price-range-both').css('display', 'none');
-	},
-    create: function(event, ui){
-    	$('.ui-slider-handle:eq(0)').append('<span class="price-range-min value">' + 0 + '</span>');
-		$('.ui-slider-handle:eq(1)').append('<span class="price-range-max value">' + 10 + '</span>');
-    }
-});
+$("#search-form")
+		.submit(
+				function(e) {
+					$('ul.list-group').html("");
+					e.preventDefault();
+					query = $("#search-form input").val().toLowerCase();
+
+					min_rating = $(".ui-slider-handle:eq(0)").text();
+					max_rating = $(".ui-slider-handle:eq(1)").text();
+
+					start_date = $(".filter-start-date").val();
+					end_date = $(".filter-end-date").val();
+
+					lang = $(".filter-language").val();
+
+					content_rating = $(".filter-content-rating").val();
+
+					country = $(".filter-country").val();
+
+					$
+							.ajax({
+								type : 'GET',
+								url : "http://localhost:8080/MovieDatabase/rest/movies/filter?yearfrom="
+										+ start_date
+										+ "&yearto="
+										+ end_date
+										+ "&ratingfrom="
+										+ min_rating
+										+ "&ratingto="
+										+ max_rating
+										+ "&keywords="
+										+ query
+										+ "&content_rating="
+										+ content_rating
+										+ "&language=" + lang,
+								success : function(data) {
+									for (var i = 0; i < data.length; i++) {
+										$('ul.list-group')
+												.append(
+														'<li class="list-group-item"><span class="movie-title">'
+																+ data[i].title
+																+ '</span><span class="label label-primary">'
+																+ data[i].year
+																+ '</span><span class="label label-success">'
+																+ data[i].score
+																+ '</span><span class="label label-info">'
+																+ data[i].language
+																+ '</span><span class="label label-warning">'
+																+ data[i].genres
+																+ '</span><a class="imdb-link" href="'
+																+ data[i].imdb
+																+ '"><img class="imdb-logo" src="imdb.png"/></a></li>');
+									}
+
+								},
+							});
+				});
+
+$('#slider')
+		.slider(
+				{
+					range : true,
+					min : 0,
+					max : 10,
+					values : [ 0, 10 ],
+					slide : function(event, ui) {
+						$('.ui-slider-handle:eq(0) .price-range-min').html(
+								'' + ui.values[0]);
+						$('.ui-slider-handle:eq(1) .price-range-max').html(
+								'' + ui.values[1]);
+						$('.price-range-both').html(
+								'<i>' + ui.values[0] + '  </i>' + ui.values[1]);
+
+						$('.price-range-min, .price-range-max').css('opacity',
+								'1');
+						$('.price-range-both').css('display', 'none');
+					},
+					create : function(event, ui) {
+						$('.ui-slider-handle:eq(0)').append(
+								'<span class="price-range-min value">' + 0
+										+ '</span>');
+						$('.ui-slider-handle:eq(1)').append(
+								'<span class="price-range-max value">' + 10
+										+ '</span>');
+					}
+				});
